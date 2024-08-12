720f4b8219049e457fe8bacbb8d098dca7b70fab
kabir-afk <kabirsingh12b@gmail.com>
2024-08-13 01:53:45 +0530

    style : formatted appearance

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,46 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { HashRouter, Routes, Route } from "react-router-dom";
 import Transcript from "../pages/Transcript";
 import Summary from "../pages/Summary";
 import reportWebVitals from "../reportWebVitals";
-import {  Tabs, TabList, TabPanels, Tab, TabPanel,ChakraProvider } from "@chakra-ui/react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  ChakraProvider,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <ChakraProvider>
-      {/* <HashRouter>
-        <Flex bgColor={'tomato'} color={'blanchedalmond'} w={'300px'}>
-            <nav>
-                <ul>
-                  <li>
-                    <a href="#/">Transcript</a>
-                  </li>
-                  <li>
-                    <a href="#/summary">Summary</a>
-                  </li>
-                </ul>
-            </nav>
-        </Flex>
-        <Routes>
-          <Route index element={<Transcript />} />
-          <Route path="summary" element={<Summary />} />
-        </Routes>
-      </HashRouter> */}
-      <Tabs variant='soft-rounded' colorScheme='green'>
-  <TabList>
-    <Tab>Transcript</Tab>
-    <Tab>Summary</Tab>
-  </TabList>
-  <TabPanels>
-    <TabPanel>
-        <Transcript/>
-    </TabPanel>
-    <TabPanel>
-      <Summary/>
-    </TabPanel>
-  </TabPanels>
-</Tabs>
+      <Tabs variant="soft-rounded" colorScheme="green">
+        <TabList>
+          <Tab>Transcript</Tab>
+          <Tab>Summary</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Transcript />
+          </TabPanel>
+          <TabPanel>
+            <Summary />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </ChakraProvider>
   );
 }
