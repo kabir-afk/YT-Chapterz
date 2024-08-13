a10b8d2ea49d62c3b62428b2d7712ad3a926fc0c
kabir-afk <kabirsingh12b@gmail.com>
2024-08-14 03:30:30 +0530

    feat : jumpToTimestamp functionality added

--- src/pages/Chapter.jsx
+++ src/pages/Chapter.jsx
@@ -1,3 +1,4 @@
+/*global chrome */
 import React from "react";
 import {
   Box,
@@ -9,6 +10,32 @@ import {
 
 const Chapter = (props) => {
   const { title, points, timestamp } = props;
+  function jumpToTimestamp(time) {
+    const tsArray = time.split(":").map(Number);
+
+    let videoTime;
+    if (tsArray.length === 2) {
+      const [minutes, seconds] = tsArray;
+      videoTime = minutes * 60 + seconds;
+    } else {
+      const [hours, minutes, seconds] = tsArray;
+      videoTime = hours * 60 + minutes * 60 + seconds;
+    }
+    // Execute script to navigate to the specific timestamp
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: (videoTime) => {
+          const video = document.querySelector("video");
+          if (video) {
+            video.currentTime = videoTime;
+            video.play();
+          }
+        },
+        args: [videoTime],
+      });
+    });
+  }
   return (
     <>
       <Box maxW="32rem">
@@ -20,7 +47,12 @@ const Chapter = (props) => {
             <ListItem key={index}>{point}</ListItem>
           ))}
         </UnorderedList>
-        <Button size="lg" colorScheme="green" mt="24px">
+        <Button
+          size="lg"
+          colorScheme="green"
+          mt="24px"
+          onClick={() => jumpToTimestamp(timestamp)}
+        >
           {timestamp}
         </Button>
       </Box>
