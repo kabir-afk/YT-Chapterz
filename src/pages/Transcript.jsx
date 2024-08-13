7548357cbcbc55e2f192c246ba793730e330ec5d
kabir-afk <kabirsingh12b@gmail.com>
2024-08-13 11:06:28 +0530

    feat : buill trasncript tab

--- src/pages/Transcript.jsx
+++ src/pages/Transcript.jsx
@@ -1,8 +1,34 @@
-import React from "react";
-import { Button } from "@chakra-ui/react";
+/*global chrome */
+import React,{useState} from "react";
+import { Button, Text } from "@chakra-ui/react";
+import { YoutubeTranscript } from "youtube-transcript";
 
 const Transcript = () => {
-  return <><Button>Transcript</Button></>;
+
+  const [transcriptString,setTranscriptString] = useState('');
+
+  async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+  async function getTranscript() {
+    let tab = await getCurrentTab();
+    
+    let transcript = await YoutubeTranscript.fetchTranscript(tab.url);
+    let transcriptText;
+    transcript.forEach(element => {
+      transcriptText += element.text;
+    });
+    setTranscriptString(transcriptText);
+  }
+  return (
+    <>
+      <Button onClick={getTranscript}>Transcript</Button>
+      <Text>{transcriptString}</Text>
+    </>
+  );
 };
 
 export default Transcript;
